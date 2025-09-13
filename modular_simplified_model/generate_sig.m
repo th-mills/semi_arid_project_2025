302e43b2bac56e382a02f734e4dd06d8abbbe915
ChessieD <francescadarkins@gmail.com>
2025-09-13 16:06:20 +0100

    sig becomes the same as sig2
    Old function for sig at the bottom. Part of the periodicity update.

--- modular_simplified_model/generate_sig.m
+++ modular_simplified_model/generate_sig.m
@@ -1,3 +1,21 @@
+% function called each time step to create an overall transport matrix
+% using the constant matrices 
+function sig = generate_sig(bio, bio_max, unvegetated_transport, vegetated_transport)
+    % f is our function for assessing when to use each transport type,
+    % depending on cell content
+    % we use a row matrix and leave the transport matrices as-is to
+    % multiply each column by f(j)
+    f = heaviside(bio - 0.1*bio_max)';
+    sig = f.*vegetated_transport + (1-f).*unvegetated_transport; 
+end
+
+
+
+
+
+% ---------- OLD VERSION OF ABOVE FUNCTION ------------------------------ %
+
+%{
 function sig = generate_sig(bio, N, bio_max)
     % create empty sparse matrix with space for our (up to) seven nonzero 
     % values per row
@@ -42,3 +60,4 @@ function sig = generate_sig(bio, N, bio_max)
         %  0.0625 0.0625 0.0625]
     end
 end
+%}
