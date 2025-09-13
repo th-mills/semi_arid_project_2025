fc8b5449c043c558e89d2eb9ca5ef46f7f5d747d
ChessieD <francescadarkins@gmail.com>
2025-09-13 16:08:35 +0100

    Add periodicity

--- modular_simplified_model/propagule_availability.m
+++ modular_simplified_model/propagule_availability.m
@@ -1,4 +1,4 @@
-function av = propagule_availability(bio, prop, N, bio_max)
+function av = propagule_availability(bio, prop, N, bio_max, periodic)
     % function for propagule redistribution, very similar to resource
     % availability
     b = reshape(bio, N, N);
@@ -10,5 +10,13 @@ function av = propagule_availability(bio, prop, N, bio_max)
     for i=2:N
         av(i,:) = av(i,:) + sigs(i-1,:).*av(i-1,:);
     end
+
+    if periodic
+        % fudge attempt 1: average out top and bottom rows' availability 
+        average = (av(1,:) + av(N,:))./2;
+        av(1,:) = average;
+        av(N,:) = average;
+    end
+
     av = reshape(av, N^2, 1);
 end
